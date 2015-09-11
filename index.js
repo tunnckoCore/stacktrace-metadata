9b17b479f7569902f045bf749217f87144df1b4c
tunnckoCore <mameto_100@mail.bg>
2015-09-11 10:28:56 +0300

    implement :cat2:

---
+++ index.js
@@ -0,0 +1,36 @@
+/*!
+ * stacktrace-metadata <https://github.com/tunnckoCore/stacktrace-metadata>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+var isError = require('is-typeof-error')
+
+module.exports = function stacktraceMetadata (err) {
+  if (!isError(err)) {
+    throw new TypeError('stacktrace-metadata: expect `err` to be error object')
+  }
+  if (err && err.stack && err.stack.length > 0) {
+    var filename = cameFrom(module.parent)
+    var stack = err.stack
+    stack = stack.slice(stack.indexOf(filename))
+    stack = stack.slice(0, stack.indexOf('\n') - 1)
+    var matches = stack.match(/([^:\s]+):(\d+)(?::(\d+))$/)
+    if (matches) {
+      err.filename = matches[1]
+      err.line = Number(matches[2])
+      err.column = Number(matches[3])
+    }
+  }
+  return err
+}
+
+function cameFrom (mod) {
+  if (mod && mod.parent) {
+    return cameFrom(mod.parent)
+  }
+  return mod && mod.filename || ''
+}
---
+++ test.js
@@ -0,0 +1,16 @@
+/*!
+ * stacktrace-metadata <https://github.com/tunnckoCore/stacktrace-metadata>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var test = require('assert')
+
+test('foo', function (done) {
+  done()
+})
